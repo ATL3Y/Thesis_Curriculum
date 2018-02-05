9e17d939e16278dcbca5d960f770925ad5911151
SIOBHAN\Karen <karenmcm@usc.edu>
2018-02-04 20:34:41 -0800

    Tutorial_1 changes, push-pull script partial cut, new prefabs, materials, etc.

---
+++ Assets/Content/Scripts/Not In Build/PushPull.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushPull : MonoBehaviour
+{
+    private List<GameObject> attractees;
+
+    // dropped in from prefab in inspector
+    public VRNodeMinion leftHand;
+    public VRNodeMinion rightHand;
+    
+    public bool GlovesOn { get; set; }
+
+    private bool IsPulling = false;
+
+    public float baseSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        attractees = gameObject.GetComponent<SpawnPrefab>().prefabs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftHand.Trigger > 0.8f)
+        {
+            StartCoroutine(follow());
+        }
+
+        if (leftHand.Bumper > 0.8f)
+        {
+            StartCoroutine(flee());
+        }
+
+    }
+
+
+
+    IEnumerator follow()
+    {
+        for (int i = 0; i < attractees.Count; i++)
+        {
+            float currentDistance = Vector3.Distance(attractees[i].transform.position, leftHand.transform.position);
+            float speed = Mathf.Lerp(0.1f, 2.0f, currentDistance);
+
+            Pull(attractees[i], speed);
+        }
+        yield return null;
+    }
+
+    void Pull(GameObject pullee, float speed)
+    {
+        pullee.transform.position = Vector3.Lerp(pullee.transform.position, leftHand.transform.position, Time.deltaTime * speed);
+    }
+
+
+
+    IEnumerator flee()
+    {
+        for (int i = 0; i < attractees.Count; i++)
+        {
+            //
+        }
+        yield return null;
+    }
+
+    void Push(GameObject pushee, float speed)
+    {
+        //
+    }
+
+}
---
+++ Assets/Content/Scripts/Not In Build/SpawnPrefab.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Remember to break this by renaming the class
+public class SpawnPrefab : MonoBehaviour
+{
+
+    public GameObject PrefabToSpawn;
+    public List<GameObject> prefabs = new List<GameObject>();
+
+    public int NumberToSpawn;
+
+    public bool RandomLocation = false;
+    public float MinXSpawnLocation;
+    public float MaxXSpawnLocation;
+    public float MinYSpawnLocation;
+    public float MaxYSpawnLocation;
+    public float MinZSpawnLocation;
+    public float MaxZSpawnLocation;
+
+    public GameObject explosionPrefab; // Mispell a variable name and include that as an error
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (prefabs.Count < NumberToSpawn)
+        {
+            InstantiatePrefab();
+        }
+    }
+
+    
+    void InstantiatePrefab()
+    {
+        Vector3 prefabPosition = new Vector3 (0, 0, 0);
+        GameObject newGameObject;
+
+        if (RandomLocation == true)
+        {
+            prefabPosition = GenerateRandomPosition();
+        }
+        newGameObject = Instantiate(PrefabToSpawn, prefabPosition, Quaternion.identity);
+        prefabs.Add(newGameObject);
+
+    }
+
+    private Vector3 GenerateRandomPosition()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(MinXSpawnLocation, MaxXSpawnLocation),
+                                        Random.Range(MinYSpawnLocation, MaxYSpawnLocation),
+                                        Random.Range(MinZSpawnLocation, MaxZSpawnLocation));
+
+        return randomPosition;
+    }
+
+}
